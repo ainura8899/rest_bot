06059de848263bc7c90b26ef1855921750b77b0a
ainura8899 <sainura28@gmail.com>
2024-08-28 23:46:19 +0600

    hw6

--- handlers/group_activities.py
+++ handlers/group_activities.py
@@ -7,30 +7,28 @@ from datetime import timedelta
 group_router = Router()
 group_router.message.filter(F.chat.type != 'private')
 
-BAD_WORDS = {"дурак", "идиот", "козел"}
+BAD_WORDS = ["дурак", "идиот", "козел"]
 
+# @group_router.message(Command("ban", prefix="!"))
+# async def ban_group_user(message: types.Message):
+#     print(message.text)
+#     print(message.from_user.first_name)
+#     # reply = message.reply_to_message
+#     # if reply:
+#         await bot.ban_chat_member(
+#             chat_id=message.chat.id,
+#             user_id=reply.from_user.id,
+#             until_date=timedelta(seconds=120)
+#         )
 
-@group_router.message(Command("ban", prefix="!"))
-async def ban_group_user(message: types.Message):
-    print(message.text)
-    print(message.from_user.first_name)
-    reply = message.reply_to_message
-    if reply:
-        await bot.ban_chat_member(
-            chat_id=message.chat.id,
-            user_id=reply.from_user.id,
-            until_date=timedelta(seconds=120)
-        )
-
-
-@group_router.message()
+@group_router.message(lambda m:m.text.lower() in BAD_WORDS)
 async def filter_bad_words(message: types.Message):
-    print(message.chat.type)
-    txt = message.text
-    for word in BAD_WORDS:
-        if word in txt.lower():
-            await message.answer(
-                f"Пользователь {message.from_user.first_name} использовал запрещенное слово"
-            )
-            await message.delete()
-            break
+    await message.answer(
+         f"Пользователь {message.from_user.first_name} использовал запрещенное слово"
+    )
+    await bot.ban_chat_member(
+         chat_id=message.chat.id,
+         user_id=message.from_user.id,
+         until_date=timedelta(seconds=120)
+    )
+    await message.delete()
